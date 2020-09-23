7c3a4acdf9842947ecf0d0a255245c2c7f83bc0f
khbznia <khabaznia@gmail.com>
2020-09-23 14:45:33 +0200

    10 files modified

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
@@ -11,13 +12,15 @@ import { HEROES } from './mock-heroes';
 
 export class HeroService {
 
-  constructor(private messageService: MessageService){
-
-  }
+  constructor(private messageService: MessageService){}
 
   getHeros(): Observable<Hero[]>{
     this.messageService.add('HeroSerice: fetched heroes');
     return of(HEROES);
   }
 
+  getHero(id: number): Observable<Hero>{
+    this.messageService.add(`HeroSerice: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
